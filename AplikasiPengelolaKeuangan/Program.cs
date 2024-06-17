32a4309ce3f623631aa6b390eeb8b892b1d5e40b
ammrbhlwn <150604655+ammrbhlwn@users.noreply.github.com>
2024-06-17 19:30:15 +0700

    initial commit

---
+++ AplikasiPengelolaKeuangan/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AplikasiPengelolaKeuangan;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("========================================================");
+        Console.WriteLine("Aplikasi Pengelola Keuangan Berbasis CLI");
+        Console.WriteLine("1. Daftar");
+        Console.WriteLine("2. Masuk");
+        Console.WriteLine("3. Keluar");
+        Console.WriteLine("Silahkan Masukkan Pilihan [1/2/3]");
+        Console.WriteLine("========================================================");
+        string pilihan = Console.ReadLine();
+
+        if (pilihan == "1")
+        {
+            DashboardAwal.Daftar();
+        }
+        else if (pilihan == "2")
+        {
+            DashboardAwal.Masuk();
+        }
+        else if (pilihan == "3")
+        {
+            Console.WriteLine("\nSampai Bertemu Kembali!!\n");
+        }
+        else
+        {
+            Console.WriteLine("\nInput Tidak Valid!!\n");
+            Main();
+        }
+    }
+}
